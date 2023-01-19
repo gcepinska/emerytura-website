33b5955fe5812ffdb3ef3e57d2ba944391073e39
gcepinska <gcepinska@gmail.com>
2023-01-19 19:19:56 +0100

    test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Emerytura czy to już?</h1>
+        <h1>Emerytura czy to już?</h1>
       </header>
     </div>
   );
